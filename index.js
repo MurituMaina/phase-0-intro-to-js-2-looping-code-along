17a279ae1f33b7d590363809b85aefc99c120377
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-04 15:04:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -2,17 +2,23 @@
 
 const name = [];
 const messages =[];
+// let re =[];
 function writeCards(name,event) {
-  for (let i = 0; i < name.length; i++) {
-    let re= `Thank you, ${name[i]}  for the wonderful,${event} gift! `;
-    
-    messages.push(re);
+//   for (let i = 0; i < name.length; i++) 
+  let i =0;
+  while(i<name.length)
+  {
+    messages.push( `Thank you, ${name[i++]}  for the wonderful,${event} gift! `);
       }
-      console.log(messages);
+    console.log(messages);
+//     AssertionError: expected undefined to deeply equal [ …(3) ]
+// at Context.<anonymous> (test/indexTest.js:17:77)
+
+    // re.push(messages);
+    // console.log(re);
 }
 
 writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
-
 
 function countDown (){
     let i =0;
