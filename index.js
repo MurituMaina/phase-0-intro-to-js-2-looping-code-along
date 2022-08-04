57678a3bbf8baebf47034111c2329722d7b38ba0
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-04 14:36:04 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+// // Code your solutions in this file
+
+const name = ["Guadalupe", "Ollie", "Aki"];
+const messages =[];
+function writeCards(name,event) {
+  for (let i = 0; i < name.length; i++) {
+    let re= `Thank you, ${name[i]}  for the wonderful,${event} gift! `;
+    
+    messages.push(re);
+      }
+      console.log(messages);
+  return name;
+}
+
+writeCards(name,"surprise");
+
+
+function countDown (){
+    let i =0;
+    while (i<=10){
+        console.log(i++);
+    }
+}
+countDown();
