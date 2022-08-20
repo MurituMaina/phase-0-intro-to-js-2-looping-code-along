75bed884dc94013d2a6dd565ad36ca79d60ee528
MurituMaina <vincent.maina@student.moringaschool.com>
2022-08-20 18:32:08 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 // // Code your solutions in this file
 const names = ["Guadalupe", "Ollie", "Aki"];
 let messages = [];
+// function writeCards(names, event){
+//   let firstMessage= [];
+//   // for(let i =0; i<names.length; i++)
+//   for(let name of names) {
+//   firstMessage.push(`Thank you, ${name}, for the wonderful ${event} gift!`);
+//   }
+//  messages =[...firstMessage]
+//   console.log(messages);
+//   return messages;
+// }
+// writeCards(names, "surprise")
+
 function writeCards(names, event){
-  let firstMessage= [];
-  for(let i =0; i<names.length; i++) {
-  firstMessage.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  let m = []
+  for(let na of names){
+    m.push(`Thank you, ${na}, for the wonderful ${event} gift!`)
   }
- messages =[...firstMessage]
-  console.log(messages);
-  return messages;
-}
+  messages=[...m]
+  console.log(messages)
+  return messages
+ }
 writeCards(names, "surprise")
+// writeCards(names, "surprise")
+// writeCards(names, "surprise")
 
 function countDown (){
     let i =0;
